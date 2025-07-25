680ae522aa75c9c25063920710b4d822b84d8fb8
conght <conghthe172673@fpt.edu.vn>
2025-07-26 00:46:09 +0700

    feat: add get and create endpoints for book entity

---
+++ CoreShelf.API/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using CoreShelf.Core.Entities;
+using CoreShelf.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreShelf.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController(CoreShelfDbContext context) : ControllerBase
+    {
+        [HttpGet] //api/books
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await context.Books.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")] //api/books/2
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            context.Books.Add(book);
+
+            await context.SaveChangesAsync();
+
+            return book;
+        }
+    }
+}
