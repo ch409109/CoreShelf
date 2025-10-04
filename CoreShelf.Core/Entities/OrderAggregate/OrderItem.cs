11c7ea403f81f58e48ce8224a58ca60b1940acad
Hoang Cong <ch409109@gmail.com>
2025-10-05 00:22:48 +0700

    feat: add Order Aggregate entities

---
+++ CoreShelf.Core/Entities/OrderAggregate/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreShelf.Core.Entities.OrderAggregate
+{
+    public class OrderItem : BaseEntity
+    {
+        public ProductItemOrdered ItemOrdered { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
