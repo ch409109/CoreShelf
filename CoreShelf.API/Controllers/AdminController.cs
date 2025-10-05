b165e3f5a8b3ec154bd4f017e8da57e13e5313a1
Hoang Cong <ch409109@gmail.com>
2025-10-05 12:07:48 +0700

    feat: adding Admin Controller into project

---
+++ CoreShelf.API/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using CoreShelf.Core.Entities.OrderAggregate;
+using CoreShelf.Core.Specifications;
+using CoreShelf.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreShelf.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController(UnitOfWork unit) : BaseApiController
+    {
+        [HttpGet("orders")]
+        public async Task<ActionResult<Order>> GetOrders([FromQuery] OrderSpecParams specParams)
+        {
+            var spec = new OrderSpecification(specParams);
+
+            return await CreatePagedResult(unit.Repository<Order>(), spec, specParams.PageIndex, specParams.PageSize);
+        }
+    }
+}
--- CoreShelf.Core/Specifications/BookSpecParams.cs
+++ CoreShelf.Core/Specifications/BookSpecParams.cs
@@ -6,17 +6,8 @@ using System.Threading.Tasks;
 
 namespace CoreShelf.Core.Specifications
 {
-    public class BookSpecParams
+    public class BookSpecParams : PagingParams
     {
-        private const int MaxPageSize = 50;
-        public int PageIndex { get; set; } = 1;
-
-        private int _pageSize = 6;
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
         private List<string> _categories = [];
 
         public List<string> Categories
